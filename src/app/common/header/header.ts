cc23d9f726fb001a846ec4b97c8cf416f684350c
Loic <balhloic@gmail.com>
2025-09-17 01:51:01 +0200

    Construction de l'en-tête et du pied de page

--- src/app/common/header/header.ts
+++ src/app/common/header/header.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule, DatePipe } from '@angular/common';
+import { Component, LOCALE_ID , OnInit } from '@angular/core';
+import { CommonModule, registerLocaleData , DatePipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+import localeFr from '@angular/common/locales/fr';
+
+
+registerLocaleData(localeFr);
 
 @Component({
   selector: 'app-header',
   imports: [CommonModule, RouterModule, DatePipe],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-FR' }
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header implements OnInit {
-  menuOpen = false;
   today = new Date();
-
-  toggle() {
-    this.menuOpen = !this.menuOpen;
-  }
 
   ngOnInit() {
     // Update the date every minute
